022440a350f23f8ab6a1220269ba3736c291646d
robbie <robbiekruszynski@gmail.com>
2021-08-05 13:55:00 -0500

    add bootcamp prep

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -6,9 +6,15 @@ module.exports = {
         const channel = '851913774343520266'
         const academy = message.guild.roles.cache.find(role => role.name === "Academy Member");
         const metaMask = message.guild.roles.cache.find(role => role.name === "MetaMask Member");
+        const bootcamp = message.guild.roles.cache.find(role => role.name === "Bootcamp Member");
+
 
         const academyEmoji = '🧑🏽‍🎓';
         const metaMaskEmoji = '🦊';
+        const bootcampEmoji = '💡';
+        //const infuraEmoji = '';
+        //const 
+        //add custom emoji based on products 
 
         let embed = new Discord.MessageEmbed()
             .setColor('#e42643')
@@ -16,10 +22,14 @@ module.exports = {
             .setDescription('Choosing a product team emoji will give you the abilitiy to view their channels along with interact with other memebers who have the same role!\n\n'
                 + `${academyEmoji} for the ConsenSys Academy channels\n`
                 + `${metaMaskEmoji} for MetaMask channels`);
+                + `${bootcampEmoji} for Bootcamp channels`);
+
 
         let messageEmbed = await message.channel.send(embed);
         messageEmbed.react(academyEmoji);
         messageEmbed.react(metaMaskEmoji);
+        messageEmbed.react(bootcampEmoji);
+
 
     client.on('messageReactionAdd', async (reaction, user) => {
         if (reaction.message.partial) await reaction.message.fetch();
